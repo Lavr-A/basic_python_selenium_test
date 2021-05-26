129ef8dcb127b913ec67510ae7472137d61bbdca
Lavr-A <79139589+Lavr-A@users.noreply.github.com>
2021-05-26 15:58:51 +0300

    Update Test_2.py

--- Test_2.py
+++ Test_2.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
-driver = webdriver.Chrome ("D:\Лекции IT Academy\Chrome\chromedriver.exe")
+driver = webdriver.Chrome ("D:\\chromedriver.exe")
 driver.get ("https://wikipedia.org/")
 
-search_field = driver.find_element_by_id("searchInput").send_keys("Space")
+search_field = driver.find_element_by_id("searchInput").send_keys("Test Automation")
 search_button = driver. find_element_by_xpath("//form[@id = 'search-form']/fieldset /button")
 search_button.click()
 
-assert "Space" in driver.title
+element_field = driver.find_element_by_xpath("//a[@href='/wiki/Selenium']")
+element_field.click()
+assert "Selenium" in driver.title
 driver.quit()
